a0cd850375ea2ed0a50d0b9334055d3b513d651b
Sam-Mauris Yong <mauris@users.noreply.github.com>
2016-03-02 10:42:05 +0800

    Create largeNumberMultiplication.js

---
+++ divide-and-conquer/largeNumberMultiplication.js
@@ -0,0 +1,11 @@
+
+/*
+ * The problem:
+ * Perform O(n^1.585) multiplcation of two large numbers of n digits.
+ *
+ * Run it at https://repl.it/Brl0
+ */
+ 
+var multiply = function(a, b) {
+  // TODO
+};
